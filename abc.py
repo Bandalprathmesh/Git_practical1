4f22087f3fa4a27a83ff8817bc3666fa4850ba97
Bandalprathmesh <prathmesh0399@gmail.com>
2022-03-10 12:36:59 +0530

    added 4th commit

--- abc.py
+++ abc.py
@@ -2,10 +2,10 @@ hungry = input("are you hungry?")
 if hungry=='yes':
     print('Pizzza')
     print('Burger') 
-elif hungry=='y':
-    print("y")
 elif hungry=='n':
-    print("n") 
+    print("n")
+elif hungry=='a':
+    print("a")
+
 else:
     print("That's better")
-    
